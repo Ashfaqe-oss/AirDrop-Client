c4a3c14149eb7acb0e0719d14eb46daf9f24eae2
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2022-08-19 11:27:46 +0530

    minor updates

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,19 +3,16 @@ import { BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
-const ServiceCard = ( { color, title, icon, subtitle } ) => (
-  <div className='flex flex-row justify-start items-center white-glassmorphism p-3 m-5 cursor-pointer hover:shadow-xl'>
-
-    <div className={`w-10 h-10 rounded-full flex justify-center items-center ${ color }`}>
+const ServiceCard = ({ color, title, icon, subtitle }) => (
+  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-5 cursor-pointer hover:shadow-xl">
+    <div
+      className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
+    >
       {icon}
     </div>
     <div className="ml-5  flex flex-col flex-1">
-      <h3 className="mt-2 text-white text-lg">
-        {title}
-      </h3>
-      <p className="mt-2 text-slate-300 text-sm font-light md:w-9/12">
-        {subtitle}
-      </p>
+      <h3 className="mt-2 text-zinc-200 text-lg">{title}</h3>
+      <p className="mt-2 text-zinc-300 text-sm italic md:w-9/12">{subtitle}</p>
     </div>
   </div>
 );
@@ -26,11 +23,11 @@ const Services = () => {
       <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
           <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
-            Servies that we
+            Services that we
             <br />
             always improve
           </h1>
-          <p className="test-left my-2 text-slate-100 font-light md:w-9/12 w-11/12 text-base">
+          <p className="test-left my-2 text-zinc-300 font-medium md:w-9/12 w-11/12 text-base">
             The best choice for buying and selling your crypto assets, with the
             various super friendly services we offer
           </p>
@@ -61,6 +58,6 @@ const Services = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Services
