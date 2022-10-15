fb39a6e457d69c8e09126c0328327c5d1a976362
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2022-10-15 20:45:22 +0530

    starting today build

--- src/components/DosaSwap.jsx
+++ src/components/DosaSwap.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function DosaSwap() {
   return (
-    <div className="ml-5  flex flex-col flex-1 m-7 p-5 mt-0">
-      <h3 className="mt-2 font-mono text-zinc-200 text-lg">DosaSwap</h3>
-      <p className="mt-2 text-zinc-300 text-sm italic md:w-9/12">upcoming . . .</p>
+    <div className="ml-5  flex flex-col flex-1 items-center justify-center m-7 p-5 mt-0 h-screen">
+      <h3 className="mt-2 font-mono text-zinc-200 text-xl">DosaSwap</h3>
+      {/* <p className="mt-2 text-zinc-300 text-sm italic md:w-9/12">upcoming . . .</p> */}
     </div>
   )
 }
