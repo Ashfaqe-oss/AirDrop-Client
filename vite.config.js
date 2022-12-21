b259c26d6279642a38a3f7f9aa271ce9f5abf195
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2022-12-21 18:17:03 +0530

    turned into PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react(), VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+            enabled: true
+        },
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+            name: 'Airdrop App',
+            short_name: 'Airdrop',
+            description: 'Airdrop and Swappig Web3.0 application',
+            theme_color: '#000',
+            icons: [{
+                    src: 'android-chrome-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: 'android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                },
+                {
+                    src: 'android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable'
+                }
+
+            ]
+        }
+    })],
+});
+
+
+// With this minimal configuration of the vite-plugin-pwa plugin, your application is now 
+// able to generate the Web App Manifest and inject it at the entry point, generate the 
+// service worker and register it in the browser.
